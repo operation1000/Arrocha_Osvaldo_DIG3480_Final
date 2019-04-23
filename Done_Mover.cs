fbc63b465b12d47e65e30be6a61087c853dc4167
operation1000 <47093160+operation1000@users.noreply.github.com>
2019-04-23 18:33:33 -0400

    Update Done_Mover.cs

--- Done_Mover.cs
+++ Done_Mover.cs
@@ -1,29 +1,12 @@
 using UnityEngine;
 using System.Collections;
 
-public class Done_WeaponController2 : MonoBehaviour
+public class Done_Mover : MonoBehaviour
 {
-	public GameObject shot;
-	public Transform shotSpawn;
-    public Transform shotSpawn2;
-    public Transform shotSpawn3;
-    public float fireRate;
-	public float delay;
-    
-    void Start()
-    {
-        InvokeRepeating("Fire", delay, fireRate);
-        InvokeRepeating("Fire2", delay, fireRate);
-    }
+	public float speed;
 
-    void Fire()
-    {
-        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-        GetComponent<AudioSource>().Play();       
-    }
-    void Fire2()
-    {   
-        Instantiate(shot, shotSpawn2.position, shotSpawn2.rotation);
-        Instantiate(shot, shotSpawn3.position, shotSpawn3.rotation);
-    }
+	void Start ()
+	{
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+	}
 }
